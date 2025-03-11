3781595230e6794525920a5c846c65dc92cb9c2a
yranjan06 <ykranjan09@gamil.com>
2025-03-11 22:23:06 +0530

    workig

--- frontend/utils/store.js
+++ frontend/utils/store.js
@@ -1,44 +1,24 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+const store = new Vuex.Store({
+    state : {
+        // like data in Vue instance
+        auth_token : null,
+        role : null,
+        loggedIn : false,
+        user_id : null,
 
-Vue.use(Vuex);
+    },
+    mutations : {
+        // function that change state
+        setUser(state, user){
+            state.auth_token = user.token
+            state.role = user.role
+            state.loggedIn = true
+            state.user_id = user.id
+        }
+    },
+    actions : {
+        // action commit mutations can be async
+    }
+})
 
-export default new Vuex.Store({
-  state: {
-    user: null,
-    token: null,
-  },
-  mutations: {
-    SET_USER(state, user) {
-      state.user = user;
-    },
-    SET_TOKEN(state, token) {
-      state.token = token;
-    },
-    LOGOUT(state) {
-      state.user = null;
-      state.token = null;
-    }
-  },
-  actions: {
-    async login({ commit }, credentials) {
-      try {
-        const response = await axios.post('/login', credentials);
-        const userData = response.data;
-        commit('SET_USER', userData);
-        commit('SET_TOKEN', userData.token);
-        return userData;
-      } catch (error) {
-        throw error.response ? error.response.data : error;
-      }
-    },
-    logout({ commit }) {
-      commit('LOGOUT');
-    }
-  },
-  getters: {
-    isAuthenticated: state => !!state.token,
-    currentUser: state => state.user
-  }
-});
+export default store
